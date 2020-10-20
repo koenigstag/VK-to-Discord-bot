88a0745444fb13ec5b4b48efae42be8dfc8be047
koenigstag <50256886+koenigstag@users.noreply.github.com>
2020-10-20 17:34:16 +0300

    0.2b

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
-token = 'token'
-ID = 'Id'
-secret = 'SFSfsffesr22fe'
-group_id = '1251224'
-greeting = 'Здравствуйте'
+
+# Configuration
+# discord part
+bot_token = '' # https://discord.com/developers/applications/ , then select your application (or create), then select 'Bot' menu option, click copy below the 'Token' heading
+
+start_greeting = 'Здравствуйте'
+
+# VK BOT
+# Инициализация
+vk_group_id = '123345123'
+access_token = '' # https://vkhost.github.io/ , Prefer VK API, or Kate Mobile links
+vkapi_version = '5.124'
+
+# задержка между проверками новых постов
+check_delay = (1*60)
